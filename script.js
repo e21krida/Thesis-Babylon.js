e196f13424ae16ffea821231a7509d91c23b41c4
e21krida <e21krida@student.his.se>
2024-03-13 13:40:34 +0100

    Implemented basic model in Babylon.js
    Imported previous experimental code to render and present boxes in Babylon.js, to test the ability of this.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 import * as BABYLON from 'babylonjs';
-
 console.log("Script loaded");
 function generateCanvases() {
   const canvasContainer = document.querySelector('.canvas-container');
@@ -23,8 +22,29 @@ function generateCanvases() {
   }
 }
 
-function generateBabylon(canvas) {
+function generateBabylon(canvasId) {
+  const canvas = document.getElementById(canvasId);
+  const engine = new BABYLON.Engine(canvas, true);
+  const scene = new BABYLON.Scene(engine);
+  scene.clearColor = new BABYLON.Color3.FromHexString('#bccbe3');
+  const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+  const box = BABYLON.MeshBuilder.CreateBox("box", { size: 1 }, scene);
+  const material = new BABYLON.StandardMaterial("material", scene);
+  material.diffuseColor = new BABYLON.Color3.FromHexString('#fc7526');
+  box.material = material;
+  const camera = new BABYLON.ArcRotateCamera("arcCamera", 0, 0, 10, BABYLON.Vector3.Zero(), scene);
+  camera.attachControl(canvas, true);
+  scene.activeCamera = camera;
+  camera.target = box.position;
 
+  function animate() {
+    requestAnimationFrame(animate);
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.01;
+    scene.render();
+  }
+
+  animate();
 }
 
 window.onload = function () {
